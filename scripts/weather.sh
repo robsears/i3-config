0cb64838dabff8c27e2d23f272db4874f7c5bd47
Rob Sears <secure@robsears.com>
2019-02-15 10:06:31 -0600

    Improve weather reporting

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -11,5 +11,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 DATA="${DIR}/../data"
 
-# Cache the weather information:
-echo "$(curl -s 'wttr.in/?1QT' | grep -m1 '°F' | sed -r 's/.* (-?[0-9]+)-?.*/\1/')" > "${DATA}/weather"
+# Cache the weather information.
+# Data from wttr.in can be either a single value, or a range matching
+# -?[0-9]+-?[0-9]+. The line below will take either input; single values will be
+# left unchanged, while ranges will be averaged
+echo "$(curl -s 'wttr.in/?1QT' | grep -m1 '°F' | sed -r 's/.* ([-_0-9]{1,}) .*/\1/' | sed -r 's/(-?[0-9]{1,})\-(\-?[0-9]{1,})/\(\1+\2\)\/2/' | bc)" > "${DATA}/weather"
+
