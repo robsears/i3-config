7df7ff3f152cef700b05a78e96a996a3bc1c437d
Rob Sears <secure@robsears.com>
2025-11-03 16:21:54 -0600

    expose module, add speedtest

--- flake.nix
+++ flake.nix
@@ -8,21 +8,13 @@
   outputs =
     inputs@{ self, nixpkgs }:
     let
-      # Since this is just configuration files, we don't need per-system outputs
-      # We'll use a dummy system just to get access to pkgs
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-
-      # plumb in various packages from platform and other inputs via overlays
       overlays = import ./nix/overlays.nix inputs;
-
       forAllSystems =
         f:
         nixpkgs.lib.genAttrs systems (
@@ -40,8 +32,15 @@
         );
     in
     {
-
       packages = forAllSystems (args: import ./nix/packages.nix args);
 
+      nixosModules.default =
+        { config, pkgs, ... }:
+        {
+          environment.systemPackages = with pkgs; [
+            self.packages.${pkgs.system}.default
+            speedtest-cli
+          ];
+        };
     };
 }
