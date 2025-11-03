fa0c0b3904c0bc9fcda41eb5674ded42ba6195d9
Rob Sears <secure@robsears.com>
2025-11-03 14:40:58 -0600

    wrap this in a flake

---
+++ nix/packages.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+let
+  default = pkgs.stdenvNoCC.mkDerivation {
+    pname = "i3-config";
+    version = "1.0.0";
+
+    src = ./.;
+
+    # No build phase needed for configuration files
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/data
+      mkdir -p $out/scripts
+
+      cp    ${../config} $out/config
+      cp -r ${../data}/* $out/data/
+      cp -r ${../scripts}/* $out/scripts/
+
+      runHook postInstall
+    '';
+
+    meta = with pkgs.lib; {
+      description = "Personal i3 window manager configuration";
+      platforms = platforms.all;
+    };
+  };
+in
+{
+  inherit default;
+}
